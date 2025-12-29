8196f1307b593bae36e91bfcbc527dda7a42fe06
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2025-12-29 22:18:47 +0000

    add project files

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TG_TOKEN")
+CHANNEL_ID = os.getenv("TG_CHANNEL_ID")
+
+AI_PROVIDER_1_KEY = os.getenv("AI1_KEY")
+AI_PROVIDER_2_KEY = os.getenv("AI2_KEY")
+
+TIMEZONE = "UTC"
