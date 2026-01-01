d8d37f4bd443ceee2d125438ab7fbfdad80e7e09
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 16:24:41 +0900

    Add fetch_macro_events function to collect macro data

---
+++ collectors/macro.py
@@ -0,0 +1,8 @@
+def fetch_macro_events():
+    return [
+        {
+            "type": "CPI",
+            "description": "US CPI came above expectations",
+            "reaction": "Risk assets sold off"
+        }
+    ]
