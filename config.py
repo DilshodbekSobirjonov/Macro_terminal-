0a8d291b59f719af6e1bf3fed860f1f80df765e7
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 20:28:36 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TG_TOKEN")
 CHANNEL_ID = os.getenv("TG_CHANNEL_ID")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 
 AI_PROVIDER_1_KEY = os.getenv("AI1_KEY")
 AI_PROVIDER_2_KEY = os.getenv("AI2_KEY")
