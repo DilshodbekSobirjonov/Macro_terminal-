c3d8f748b0757eb6d4ab05ab8d39c8d4e35bfd14
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 20:19:33 +0900

    Update publisher.py

--- news/publisher.py
+++ news/publisher.py
@@ -1,4 +1,16 @@
+# news/publisher.py
 from tg.client import send_channel_message
 
 def publish_news(text):
-    send_channel_message(text)
+    """
+    Publish market news in monospace (terminal style)
+    """
+    message = f"<pre>{text}</pre>"
+    send_channel_message(message, parse_mode="HTML")
+
+def publish_status(text):
+    """
+    Publish market status in monospace (terminal style)
+    """
+    message = f"<pre>{text}</pre>"
+    send_channel_message(message, parse_mode="HTML")
