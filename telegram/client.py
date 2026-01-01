437f1c78834ad8d53d611fe8ff6abf0cbdb0f9a1
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 16:30:16 +0900

    Update client.py

--- telegram/client.py
+++ telegram/client.py
@@ -1,7 +1,7 @@
 from telegram import Bot
 from config import TELEGRAM_TOKEN, CHANNEL_ID
 
-bot = Bot(token=8543769576:AAGmG7nQjuezf5OqHqzAWIZI1GJsP3b_Gyw)
+bot = Bot(token=TELEGRAM_TOKEN)
 
 def send_message(text):
-    bot.send_message(chat_id=-1003534899420, text=text)
+    bot.send_message(chat_id=CHANNEL_ID, text=text)
