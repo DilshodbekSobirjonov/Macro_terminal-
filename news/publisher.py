09332023abf71683fb50f517fb4592824cb0ab8a
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 21:06:23 +0900

    Update publisher.py

--- news/publisher.py
+++ news/publisher.py
@@ -1,16 +1,19 @@
 # news/publisher.py
-from tg.client import send_channel_message
+from tg.client import send_channel_message, pin_message, unpin_all
 
 def publish_news(text):
-    """
-    Publish market news in monospace (terminal style)
-    """
     message = f"<pre>{text}</pre>"
     send_channel_message(message, parse_mode="HTML")
 
 def publish_status(text):
-    """
-    Publish market status in monospace (terminal style)
-    """
     message = f"<pre>{text}</pre>"
-    send_channel_message(message, parse_mode="HTML")
+
+    # отправляем статус
+    msg = send_channel_message(message, parse_mode="HTML")
+
+    # снимаем старый pin и ставим новый
+    try:
+        unpin_all()
+        pin_message(msg.message_id)
+    except Exception as e:
+        print(f"[PIN ERROR] {e}")
