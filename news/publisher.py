80e4153ee135923a175f37db9c54c84fdaf3d27c
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-01 17:17:15 +0900

    Update import statement for send_message function

--- news/publisher.py
+++ news/publisher.py
@@ -1,4 +1,4 @@
-from telegram.client import send_message
+from tg.client import send_message
 from storage.state import can_publish_news, increment_news
 
 def publish_news(text):
@@ -6,3 +6,4 @@ def publish_news(text):
         return
     send_message(text)
     increment_news()
+
